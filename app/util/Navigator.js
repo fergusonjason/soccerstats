07dff9071c85e025e46e7168166c67329d558cd5
Jason <fergusonjason@gmail.com>
2018-06-25 17:55:41 -0400

    added AddDivisionScreen, TeamManagementScreen

--- app/util/Navigator.js
+++ app/util/Navigator.js
@@ -1,10 +1,12 @@
 import {createStackNavigator} from "react-navigation";
 
 import HomeScreen from "./../screens/HomeScreen";
-import DivisionManagementScreen from "./../screens/LeagueManagement/DivisionManagement";
+import DivisionManagementScreen from "./../screens/DivisionManagement";
 import StaffManagementScreen from "./../screens/StaffManagement";
 import EditStaffMemberScreen from "./../screens/EditStaffMemberScreen";
 import AddStaffMemberScreen from "./../screens/AddStaffMember";
+import AddDivisionScreen from "./../screens/AddDivision";
+import TeamManagementScreen from "./../screens/TeamManagement";
 
 // create the navigator
 
@@ -13,7 +15,9 @@ const Navigator = createStackNavigator({
     DivisionManagementScreen: {screen: DivisionManagementScreen, navigationOptions: {title: "Division Management"}},
     StaffManagementScreen: {screen: StaffManagementScreen, navigationOptions: {title: "League Staff"}},
     EditStaffMemberScreen: {screen: EditStaffMemberScreen, navigationOptions: {title: "Edit Staff Member"}},
-    AddStaffMemberScreen: {Screen: AddStaffMemberScreen, navigationOptions: {title: "Add Staff Member"}}
+    AddStaffMemberScreen: {screen: AddStaffMemberScreen, navigationOptions: {title: "Add Staff Member"}},
+    AddDivisionScreen: {screen: AddDivisionScreen, navigationOptions: {title: "Add Division"}},
+    TeamManagementScreen: {screen: TeamManagementScreen, navigationOptions: {title: "Team Management"}}
 });
 
 export default Navigator;
