f71214bb5f1878771d79d433ce79406e5df6eb9d
Jason Ferguson <fergusonjason@gmail.com>
2018-06-26 14:21:26 -0400

    Implemented _deleteDivision

--- app/screens/DivisionManagement/DivisionManagementRow.js
+++ app/screens/DivisionManagement/DivisionManagementRow.js
@@ -3,6 +3,9 @@ import {View, Text, Alert, TouchableOpacity, StyleSheet} from "react-native";
 import {withNavigation} from "react-navigation";
 import PropTypes from "prop-types";
 
+import {open,query, execute, close} from "./../../util/DbUtils";
+
+
 class DivisionManagementRow extends Component {
 
     constructor(props) {
@@ -18,7 +21,7 @@ class DivisionManagementRow extends Component {
     }
 
     _btnEditDivision = () => {
-
+        this.props.navigation.navigate("EditDivisionScreen", {divisionId: this.state.division.DIVISION_ID});
     }
 
     _btnDeleteDivision = () => {
@@ -27,24 +30,30 @@ class DivisionManagementRow extends Component {
             "This will delete all teams that are assigned to this division. Are you sure you want to do this?",
             [
                 {text: "Cancel"},
-                {text: "Ok", onPress: this._deleteDivision}
+                {text: "Ok", onPress: () => this._deleteDivision()}
             ]
         );
     }
 
-    _deleteDivision = () => {
+    _deleteDivision = async () => {
 
-    }
+        console.log("Entered _deleteDivision, divisionId: " + this.state.division.DIVISION_ID);
+        const sql = "DELETE FROM DIVISION WHERE DIVISION_ID = ?";
 
-    componentWillMount() {
+        let db = await open({name: "stats.db",createFromLocation: "~soccerstats.db"});
+        let result = await execute(db, sql, [this.state.division.DIVISION_ID]);
 
-        console.log("State: " + JSON.stringify(this.state));
+        console.log("Rows affected: " + result.rowsAffected);
+
+        this.props.navigation.navigate("DivisionManagementScreen");
     }
 
     render() {
+        console.log("Props: " + JSON.stringify(this.props));
         return (
             <View style={styles.component}>
                 <Text>{this.state.division.item.DIVISION_NAME}</Text>
+                <View style={styles.buttonSection}>
                 <TouchableOpacity style={styles.button}
                     onPress={this._btnAddTeam}>
                     <Text style={styles.buttonText}>Teams</Text>
@@ -57,6 +66,7 @@ class DivisionManagementRow extends Component {
                     onPress={this._btnDeleteDivision}>
                     <Text style={styles.buttonText}>Delete</Text>
                 </TouchableOpacity>
+                </View>
             </View>
         );
     }
@@ -69,6 +79,14 @@ DivisionManagementRow.propTypes = {
 const styles = StyleSheet.create({
     component: {
         flexDirection: "row"
+    },
+    textSection: {
+        flexDirection: "row",
+        alignItems: "flex-start"
+    },
+    buttonSection: {
+        flexDirection: "row",
+        alignItems: "flex-end"
     },
     button: {
         alignItems: "flex-start",
