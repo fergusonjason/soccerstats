e2774209ef3cd8818eabd3e15531156ff3f7960d
Jason Ferguson <fergusonjason@gmail.com>
2018-06-28 11:00:41 -0400

    started implementing database code

--- app/screens/TeamManagement/index.js
+++ app/screens/TeamManagement/index.js
@@ -1,7 +1,48 @@
 import React, {Component} from "react";
 import {View, Text} from "react-native";
 
+import {open, close, query, execute} from "./../../util/DbUtils";
+
 class TeamManagementScreen extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            divisionId: this.props.navigation.getParam("divisionId"),
+            teams: []
+        }
+    }
+
+    componentDidMount() {
+
+        this.db = await open({name: "stats.db",createFromLocation: "~soccerstats.db"});
+    }
+
+    componentWillUnmount() {
+
+        close(this.db);
+    }
+
+    _query = async () => {
+
+        const sql = "SELECT * FROM TEAM WHERE DIVISION_ID = ?";
+        let result = await query(this.db, sql, [this.state.divisionId]);
+
+        this.setState({teams: result.result});
+    }
+
+    _btnDeleteTeam = async (teamId) => {
+
+        await _deleteTeam(teamId);
+        await _query();
+    }
+
+    _deleteTeam = async (teamId) => {
+
+        const sql = "DELETE FROM TEAM WHERE TEAM_ID = ?";
+        let result = await execute(this.db, sql, [teamId]);
+    }
 
     render() {
         return (
