dec6eb45a244df710df43431c98b72c4ed4ee470
Jason <fergusonjason@gmail.com>
2018-07-07 19:16:16 -0400

    fix text alignment in row

--- app/screens/StaffManagement/StaffManagementRow.js
+++ app/screens/StaffManagement/StaffManagementRow.js
@@ -20,7 +20,7 @@ class StaffManagementRow extends Component {
         return (
             <View style={rowStyles.rowComponent}>
                 <View style={rowStyles.rowTextSection}>
-                    <Text style={styles.text}>{this.props.staffMember.STAFF_NAME}</Text>
+                    <Text>{this.props.staffMember.STAFF_NAME}</Text>
                 </View>
                 <View style={rowStyles.rowButtonSection}>
                     <PortableButton defaultLabel="Edit"
