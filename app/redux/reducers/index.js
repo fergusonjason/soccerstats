0c1266b465dd9f0b1ee7e04ee5c37c32568f9d84
Jason <fergusonjason@gmail.com>
2018-07-31 14:42:49 -0400

    added staff management reducers

--- app/redux/reducers/index.js
+++ app/redux/reducers/index.js
@@ -2,8 +2,11 @@
 
 import {combineReducers} from "redux";
 import {divisions, division} from "./divisionReducers";
+import {staffMembers, staffMember} from "./staffReducer";
 
 export default rootReducer = combineReducers({
     divisions,
-    division
+    division,
+    staffMembers,
+    staffMember
 });
