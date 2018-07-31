dac6532df2899e495a506e77a509ef7bfd4fca02
Jason <fergusonjason@gmail.com>
2018-07-31 10:54:47 -0400

    switch over to redux/thunk

--- app/screens/EditDivision/index.js
+++ app/screens/EditDivision/index.js
@@ -2,10 +2,11 @@
 import React, {Component} from "react";
 import {View, Text, TextInput, Alert, TouchableOpacity} from "react-native";
 import {withNavigation} from "react-navigation";
-
-import {open,query, execute, close} from "./../../util/DbUtils";
+import {connect} from "react-redux";
 
 import PortableButton from "./../../components/PortableButton";
+
+import {getDivision} from "./../../redux/actions/divisionActions";
 
 import masterStyles, {dataEntryPage, bigButtonStyles} from "./../../styles/master";
 
@@ -14,44 +15,28 @@ class EditDivisionScreen extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {division: null,
-            DIVISION_ID: -1,
-            DIVISION_NAME: ""}
+        this.state = {
+            DIVISION_NAME: ""
+        };
     }
 
     async componentDidMount() {
 
         let divisionId = this.props.navigation.getParam("divisionId");
 
-        const sql = "SELECT * FROM DIVISION WHERE DIVISION_ID = ?";
+        this.props.getDivision(divisionId);
 
-        this.db = await open({name: "stats.db",createFromLocation: "~soccerstats.db"});
-        let result = await query(this.db, sql, [divisionId]);
-
-        this.setState({DIVISION_NAME: result.result[0].DIVISION_NAME, DIVISION_ID: divisionId});
     }
 
-    async componentWillUnmount() {
+    _btnUpdate = () => {
 
-        close(this.db);
-    }
+            let division = {
+                DIVISION_NAME: this.state.DIVISION_NAME
+            }
 
-    _btnUpdate = async () => {
-
-        const sql = "UPDATE DIVISION(DIVISION_NAME) VALUES(?) WHERE DIVISION_ID = ?";
-        let result = await execute(this.db, sql, [this.state.DIVISION_NAME, this.state.DIVISION_ID]);
-
-        console.log("Rows affected: " + result.rowsAffected);
-        if (result.rowsAffected == 0) {
-            Alert.alert("Database error",
-            "Error: Database update failed",
-            [
-                {text: "Ok"}
-            ]);
-        } else {
-            this.props.navigation.state.params.refresh();
+            this.props.editDivision(division);
             this.props.navigation.navigate("DivisionManagementScreen", {programId: 1});
-        }
+        //}
         
 
     }
@@ -61,11 +46,12 @@ class EditDivisionScreen extends Component {
             <View style={masterStyles.component}>
                 <View style={dataEntryPage.inputSection}>
                     <Text>Division Name</Text>
-                    <TextInput value={this.state.DIVISION_NAME} 
+                    <TextInput value={this.props.division.DIVISION_NAME} 
                         onChangeText={(text) => this.setState({DIVISION_NAME: text})}/>
                 </View>
                 <View style={dataEntryPage.bottomButtonArea}>
                     <PortableButton defaultLabel="Update Division"
+                        disabled={false}
                         onPress={() => {this_btnUpdate()}}
                         onLongPress={()=>{}}
                         style={bigButtonStyles} />
@@ -75,4 +61,17 @@ class EditDivisionScreen extends Component {
     }
 }
 
-export default withNavigation(EditDivisionScreen);
+function mapStateToProps(state) {
+    return {
+        division: state.division
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        getDivision: (divisionId) => {dispatch(getDivision(divisionId))},
+        editDivision: (division) => {dispatch(editDivision(division))}
+    }
+}
+
+export default withNavigation(connect(mapStateToProps, mapDispatchToProps)(EditDivisionScreen));
