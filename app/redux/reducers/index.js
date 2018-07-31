36f54806aa7e1a27c8d1e3f831b5873117bfcf66
Jason <fergusonjason@gmail.com>
2018-07-31 18:28:51 -0400

    add scaffolding for teams

--- app/redux/reducers/index.js
+++ app/redux/reducers/index.js
@@ -3,10 +3,13 @@
 import {combineReducers} from "redux";
 import {divisions, division} from "./divisionReducers";
 import {staffMembers, staffMember} from "./staffReducer";
+import {teams, team} from "./teamReducer";
 
 export default rootReducer = combineReducers({
     divisions,
     division,
     staffMembers,
-    staffMember
+    staffMember,
+    teams,
+    team
 });
