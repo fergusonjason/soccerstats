7a16a6c83774cda1b7cd482a9101d31aa3c8afca
Jason Ferguson <fergusonjason@gmail.com>
2018-08-01 14:48:43 -0400

    added getAllCoaches and getAllCommisioners

--- app/redux/actions/utilityActions.js
+++ app/redux/actions/utilityActions.js
@@ -1,10 +1,59 @@
 // /app/redux/actions/utilityActions
 
-import { LOADING } from "./actionTypes";
+import { LOADING, GET_COACHES_SUCCESS, GET_COMMISIONERS_SUCCESS } from "./actionTypes";
+import {queryPromise} from "./../../util/DbUtils";
+
 
 export const isLoadingData = (isLoadingBool) => {
     return {
         type: LOADING,
         isLoading: isLoadingBool
+    }
+}
+
+export const getAllCoaches = () => {
+
+    return (dispatch) => {
+        const sql = "SELECT * FROM V_COACHES";
+        const params = [];
+        dispatch(isLoadingData(true));
+        queryPromise(sql, params)
+            .then((queryResults) => {
+                dispatch(isLoadingData(false));
+                return queryResults;
+            }).then((queryResults) => {
+                console.log(`getAllCoaches result: ${JSON.stringify(queryResults)}`);
+                dispatch(getAllCoachesSuccess(queryResults.result));
+            });
+    }
+}
+
+export const getAllCoachesSuccess = (coaches) => {
+    return {
+        type: GET_COACHES_SUCCESS,
+        coaches: coaches
+    }
+}
+
+export const getAllCommisioners = () => {
+
+    return (dispatch) => {
+        const sql = "SELECT * FROM V_COMMISIONERS";
+        const params = [];
+        dispatch(isLoadingData(true));
+        queryPromise(sql, params)
+            .then((queryResults) => {
+                dispatch(isLoadingData(false));
+                return queryResults;
+            }).then((queryResults) => {
+                dispatch(getAllCoachesSuccess(queryResults.result));
+            })
+    }
+}
+
+export const getAllCommisionersSuccess = (commisioners) => {
+    return {
+        type: GET_COMMISIONERS_SUCCESS,
+        commisioners: commisioners
     }
 }
